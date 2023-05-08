54b720fbd13d8d49f94a3972d5c2528a7cad5ccf
Bart De Paepe <bart.depaepe@student.hogent.be>
2023-05-08 21:33:06 +0200

    initial commit

---
+++ src/main/java/com/springBoot_opdracht/SpringBootOpdrachtApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot_opdracht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOpdrachtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOpdrachtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springBoot_opdracht/SpringBootOpdrachtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot_opdracht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOpdrachtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
